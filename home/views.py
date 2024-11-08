49e92f0f4738669b294f85a1e10345d658f8fb12
Dibyendu Maity <60811558+dmighty007@users.noreply.github.com>
2024-11-09 01:59:55 +0530

    New home page layout

--- home/views.py
+++ home/views.py
@@ -21,20 +21,21 @@ def home(request):
     query = Colab.objects.all()
     news = News.objects.all()
     result = ""
-    if request.method == "POST":
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            send_mail(
-                form.cleaned_data["subject"],  # subject
-                f"Via website : Message from {form.cleaned_data['name']} <{form.cleaned_data['email']}>\n\n"
-                f"{form.cleaned_data['message']}",  # message
-                {form.cleaned_data["email"]},  # from email
-                ["dibyendumaity1999@bose.res.in"],  # replace with your email
-            )
-            result = "Your message has been sent!"
-            return HttpResponseRedirect(reverse("home"))
-    else:
-        form = ContactForm()
+    form = ""
+    # if request.method == "POST":
+    #     form = ContactForm(request.POST)
+    #     if form.is_valid():
+    #         send_mail(
+    #             form.cleaned_data["subject"],  # subject
+    #             f"Via website : Message from {form.cleaned_data['name']} <{form.cleaned_data['email']}>\n\n"
+    #             f"{form.cleaned_data['message']}",  # message
+    #             {form.cleaned_data["email"]},  # from email
+    #             ["dibyendumaity1999@bose.res.in"],  # replace with your email
+    #         )
+    #         result = "Your message has been sent!"
+    #         return HttpResponseRedirect(reverse("home"))
+    # else:
+    #     form = ContactForm()
     text = render(
         request,
         "home/home.html",
@@ -65,12 +66,13 @@ def team(request):
 
 def contacts(request):
     result = ""
-    if request.method == "POST":
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            return formated_mail(form)
-    else:
-        form = ContactForm()
+    form = ""
+    # if request.method == "POST":
+    #     form = ContactForm(request.POST)
+    #     if form.is_valid():
+    #         return formated_mail(form)
+    # else:
+    #     form = ContactForm()
     text = render(request, "home/contact.html", {"form": form, "result": result})
     if sync:
         with open("contacts.html", "wb") as f:
